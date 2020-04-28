0b166b56557498de3a9eb6465ddb0a7ef8207674
cristianordonez94 <62552837+cristianordonez94@users.noreply.github.com>
2020-04-28 16:52:44 -0400

    Added local files to git repo folder on desktop

---
+++ colorgame.js
@@ -0,0 +1,140 @@
+var numSquares = 12;
+var colors = [];
+var pickedColor;
+var squares = document.querySelectorAll('.square');
+var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1')
+var resetButton = document.querySelector('#reset');
+var hardBtn = document.querySelector('#hardBtn')
+var modeButtons = document.querySelectorAll('.mode');
+
+init();
+
+
+
+
+function init(){
+    //mode buttons event listeners
+    for(var i = 0; i < modeButtons.length; i++){
+        modeButtons[i].addEventListener('click', function(){
+            modeButtons[0].classList.remove('selected');
+            modeButtons[1].classList.remove('selected');
+            modeButtons[2].classList.remove('selected');
+    this.classList.add('selected');
+    if(this.textContent === 'Easy'){
+        numSquares = 3;
+    } else if (this.textContent === 'Medium'){
+        numSquares = 6;
+    } else {
+        numSquares = 12;
+    }
+    reset();
+        })
+    }
+    for(var i = 0; i < squares.length; i++){
+        //add click listeners to squares
+    squares[i].addEventListener('click', function(){
+       //grab color of clicked square
+       var clickedColor = this.style.backgroundColor;
+       //compare color to pickedColor variable 
+       if(clickedColor === pickedColor){
+          messageDisplay.textContent = 'Correct!'
+          resetButton.textContent = 'Play Again?'
+          changeColors(clickedColor);
+          h1.style.backgroundColor = clickedColor;
+       } else {
+           this.style.backgroundColor = '#232323'
+           messageDisplay.textContent = 'Try Again'
+       }
+    });
+        }
+        reset();
+}
+
+
+
+
+
+function reset(){
+    colors = generateRandomColors(numSquares);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colorDisplay to match picked color
+    colorDisplay.textContent = pickedColor; 
+   resetButton.textContent = 'New Colors';
+    messageDisplay.textContent = ''
+    //change the colors of the squares on the page 
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.display = 'block';
+            squares[i].style.backgroundColor = colors[i];
+
+        } else {
+squares[i].style.display = 'none';
+        }
+    }
+    h1.style.backgroundColor = 'steelblue';
+}
+
+ 
+
+
+
+
+
+resetButton.addEventListener('click', function(){
+    reset();
+})
+
+
+
+
+
+
+
+function changeColors(color){
+    //loop through all squares
+    for(var i = 0; i < squares.length; i++){
+          //change each color to match each given color
+          squares[i].style.background = color;
+    }
+    
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num){
+    //make an array 
+    var arr = []
+    //repeat num times
+    for(var i = 0; i < num; i++){
+        //get random color and push into array
+        arr.push(randomColor())
+        
+
+    }
+    //return that array
+    return arr;
+}
+
+function randomColor(){
+    //pick a 'red' from 0 - 255, multiply by 256 because we are rounding down and we want 255 to be greatest number possible
+   var r =  Math.floor(Math.random() * 256)
+    //pick a 'green' from 0 - 255
+   var g =  Math.floor(Math.random() * 256)
+    //pick a 'blue' from 0 - 255
+   var b =  Math.floor(Math.random() * 256)
+   
+ return `rgb(${r}, ${g}, ${b})`;
+
+}
+
+//make the block disappear 
+// document.querySelectorAll('.square')[5].style.display = 'none';
+
+//make the block reappear again 
+//document.querySelectorAll('.square')[5].style.display = 'block';
